950e7673f6f22c6cb608bc6c17c6eef7b547cebe
stevenbanilbo <steven.banilbo03@gmail.com>
2021-09-07 19:54:56 +0800

    commiy

--- src/App.js
+++ src/App.js
@@ -1,101 +1,31 @@
 import React from 'react';
+import data from './data';
+import Product from './components/Product';
 
 function App() {
   return (
-    <div class="grid-container">
-      <header class="row">
+    <div className="grid-container">
+      <header className="row">
         <div>
-          <a class="brand" href="index.html">Gee</a>
+          <a className="brand" href="/">
+           Gee
+          </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
-          <div class="row center">
-            <div class="card">
-              <a href="product.html">
-               
-                <img class="medium" src="../images/GEE_1.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Black w/ gold</h2>
-                </a>
-                <div class="price">$120</div>
-              </div>
-            </div>
-            <div class="card">
-              <a href="product.html">
-            
-                <img class="medium" src="../images/GEE_2.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Easy slip-on</h2>
-                </a>
-           
-                <div class="price">$120</div>
-              </div>
-            </div>
-            <div class="card">
-              <a href="product.html">
-         
-                <img class="medium" src="./images/GEE_3.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Pure white</h2>
-                </a>
-      
-                <div class="price">$120</div>
-              </div>
-            </div>
-            <div class="card">
-              <a href="product.html">
-                
-                <img class="medium" src="./images/GEE_4.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Easy slip-on 2</h2>
-                </a>
-    
-                <div class="price">$120</div>
-              </div>
-            </div>
-            <div class="card">
-              <a href="product.html">
-             
-                <img class="medium" src="./images/GEE_5.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Flesh</h2>
-                </a>
-    
-                <div class="price">$120</div>
-              </div>
-            </div>
-            <div class="card">
-              <a href="product.html">
-               
-                <img class="medium" src="./images/GEE_6.jpg" alt="product" />
-              </a>
-              <div class="card-body">
-                <a href="product.html">
-                  <h2>Open</h2>
-                </a>
-
-                <div class="price">$120</div>
-              </div>
-            </div>
+          <div className="row center">
+            {data.products.map((product) => (
+             <Product key={product._id} product={product}></Product>
+            ))}
           </div>
         </div>
       </main>
-      <footer class="row center">All right reserved</footer>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
