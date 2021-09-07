5a95cb355ad4750f12ba57ca613d9b2de0e32e60
stevenbanilbo <steven.banilbo03@gmail.com>
2021-09-07 17:33:04 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="index.html">Gee</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div class="row center">
+            <div class="card">
+              <a href="product.html">
+               
+                <img class="medium" src="../images/GEE_1.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Black w/ gold</h2>
+                </a>
+                <div class="price">$120</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="product.html">
+            
+                <img class="medium" src="../images/GEE_2.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Easy slip-on</h2>
+                </a>
+           
+                <div class="price">$120</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="product.html">
+         
+                <img class="medium" src="./images/GEE_3.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Pure white</h2>
+                </a>
+      
+                <div class="price">$120</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="product.html">
+                
+                <img class="medium" src="./images/GEE_4.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Easy slip-on 2</h2>
+                </a>
+    
+                <div class="price">$120</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="product.html">
+             
+                <img class="medium" src="./images/GEE_5.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Flesh</h2>
+                </a>
+    
+                <div class="price">$120</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="product.html">
+               
+                <img class="medium" src="./images/GEE_6.jpg" alt="product" />
+              </a>
+              <div class="card-body">
+                <a href="product.html">
+                  <h2>Open</h2>
+                </a>
+
+                <div class="price">$120</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer class="row center">All right reserved</footer>
     </div>
   );
 }
